0c384fdbedfa7e2b507aa0b0b510f0b3e4ddabcd
anespinosaj-un <anespinosaj@unal.edu.co>
2026-09-16 15:18:05 -0500

    Create Edades.java

---
+++ Ejercicio4/Edades.java
@@ -0,0 +1,15 @@
+class Edades {
+
+    public static double calcular_edalber(double edjuan) {
+        return 2 * edjuan / 3;
+    }
+
+    public static double calcular_edana(double edjuan) {
+        return 4 * edjuan / 3;
+    }
+
+    public static double calcular_edmama(double edjuan, double edalber, double edana) {
+        return edjuan + edalber + edana;
+    }
+}
+
