5010b039eb9d81bb2add62ad7e49a54e2d0f6c1f
anespinosaj-un <anespinosaj@unal.edu.co>
2026-09-16 15:19:56 -0500

    Create Main.java

---
+++ Ejercicio4/Main.java
@@ -0,0 +1,18 @@
+class Main {
+    public static void main(String[] args) {
+        double edjuan, edalber, edana, edmama;
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Cuantos años tiene Juan? ");
+        edjuan = scanner.nextDouble();
+
+        edalber = Edades.calcular_edalber(edjuan);
+        edana = Edades.calcular_edana(edjuan);
+        edmama = Edades.calcular_edmama(edjuan, edalber, edana);
+
+        System.out.println("la edad de la mama es: " + edmama);
+        System.out.println("la edad de juan es: " + edjuan);
+        System.out.println("la edad de albert es: " + edalber);
+        System.out.println("la edad de ana es: " + edana);
+    }
+}
