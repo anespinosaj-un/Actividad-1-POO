27199a2b90c425ce83e6f1f49851b74c2c509543
anespinosaj-un <anespinosaj@unal.edu.co>
2026-09-16 15:32:34 -0500

    Create Operaciones.java

---
+++ Ejercicio17/Operaciones.java
@@ -0,0 +1,10 @@
+public class Operaciones {
+
+    public static double calcular_longitud_circunferencia(double radio) {
+        return 2 * Math.PI * radio;
+    }
+
+    public static double calcular_area_circulo(double radio) {
+        return Math.PI * Math.pow(radio, 2);
+    }
+}
